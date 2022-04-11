28a7ce5ede039d895983d72f181d0fc9a5e003bd
volk <velvel2k@gmail.com>
2022-04-11 20:33:00 +0300

    finaasl

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -1,44 +1,34 @@
 import "./AddUser.css";
-import { useState } from "react";
+import { useRef } from "react";
 import Button from "../button/Button";
 
 const AddUser = (props) => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
+  const userName = useRef();
+  const userAge = useRef();
 
   const submitFormHandler = (event) => {
     event.preventDefault();
-    if (name.length !== 0 && +age > 0)
-      props.onAddUser({ name: name, age: age });
+    const enterdNameRef = userName.current.value;
+    const enterdAgeRef = userAge.current.value;
+    if (enterdNameRef.length !== 0 && +enterdAgeRef > 0)
+      props.onAddUser({ name: enterdNameRef, age: enterdAgeRef });
     else {
-      if (name.length === 0) {
+      if (enterdNameRef.length === 0) {
         props.onErrorInput(true, "you did not enter a name");
-      } else if (+age <= 0)
+      } else if (+enterdAgeRef <= 0)
         props.onErrorInput(true, "you did not enter a valid age");
     }
-    setAge("");
-    setName("");
-  };
-
-  const nameChangeHandler = (event) => {
-    setName(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setAge(event.target.value);
+    userName.current.value = "";
+    userAge.current.value = "";
   };
 
   return (
     <div className="form-container">
       <form id="form" onSubmit={submitFormHandler}>
         <label htmlFor="username">User Name</label>
-        <input
-          id="username"
-          type="text"
-          value={name}
-          onChange={nameChangeHandler}
-        />
+        <input id="username" type="text" ref={userName} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" value={age} onChange={ageChangeHandler} />
+        <input id="age" type="number" ref={userAge} />
         <div className="submit-button">
           <Button text={"Add User"} id="addUser" type="submit"></Button>
         </div>
